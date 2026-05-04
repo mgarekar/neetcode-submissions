cf1704ef7365526218fb02e3e33cad5bffe142e7
manav <manavgarekar@gmail.com>
2026-05-04 08:28:55 -0500

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,35 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # nums = [1,3,4,2]
+        # target = 6
+        
+        ans = []
+        i=0
+        print("hi")
+
+        while( i<len(nums) ):
+            diff = target - nums[i]
+            if diff not in nums[i+1:]: 
+                i = i+1
+                continue
+
+
+            #diff in nums, whats the index? 
+            first_number = i
+            other_number = i + 1
+            ans.append(first_number)
+
+
+            print(f'Diff is {diff}')
+            print(f'i is {i}')            
+            print(ans)
+
+            while( other_number < len(nums)  ):
+                if (nums[other_number] == diff):
+                    ans.append(other_number)
+                    return ans
+                other_number = other_number + 1
+
+
+
+
