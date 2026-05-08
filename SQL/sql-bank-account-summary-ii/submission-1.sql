18b6ff4d5e126b5114a3acc4bc232e5d6a2722e2
manav <manavgarekar@gmail.com>
2026-05-08 09:23:55 -0500

    Add: sql-bank-account-summary-ii - submission-1

---
+++ SQL/sql-bank-account-summary-ii/submission-1.sql
@@ -0,0 +1,16 @@
+-- Write your query below
+
+select 
+    u.name,x.balance
+    from users u
+    right join 
+    (select 
+            account,
+            sum(amount) as balance
+        from transactions
+        group by account
+        having sum(amount) > 10000
+    ) x
+    on x.account = u.account
+
+    where u.account IS NOT NULL
